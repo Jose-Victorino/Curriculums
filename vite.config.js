aa90f2ac9551d9274db91e5e1c66a0ffa90f214c
Jose Lorenzo L. Victorino <josevictorino003@gmail>
2026-01-21 00:49:29 +0800

    allowedHosts <hotfix>

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
-    allowedHosts: ['curriculums.onrender.com']
+    allowedHosts: ['curriculums-yevg.onrender.com']
   }
 })
